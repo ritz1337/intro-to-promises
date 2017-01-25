f20f9c53cabbffcffa2fb8aacb21525b469d3cbf
Ritwik Ruia <ritwikruia@gmail.com>
2017-01-25 13:50:31 -0800

    adds some code

--- activity.js
+++ activity.js
@@ -1,23 +1,55 @@
-// In what order will the following fire?
+// // In what order will the following fire?
 
-setTimeout( () => { console.log(1) }, 5000);
+// setTimeout( () => { console.log(1) }, 5000);
 
-setTimeout( () => { console.log(2) }, 2000);
+// setTimeout( () => { console.log(2) }, 2000);
 
-setTimeout( () => { console.log(3) }, 1000);
+// setTimeout( () => { console.log(3) }, 1000);
 
-// What does the following return?
+// // What does the following return?
+// const randomTime = () => Math.floor( Math.random() * 5000);
+
+// // In what order will the following fire?
+// setTimeout( () => { console.log(1) }, randomTime() );
+
+// setTimeout( () => { console.log(2) }, randomTime() );
+
+// setTimeout( () => { console.log(3) }, randomTime() );
+
+// // When will the number be logged?
+// const randomWait = (number) => {
+//   setTimeout( () => { console.log(number) }, randomTime() );
+// }
+
+
+//
+// with promise
+
 const randomTime = () => Math.floor( Math.random() * 5000);
 
-// In what order will the following fire?
-setTimeout( () => { console.log(1) }, randomTime() );
-
-setTimeout( () => { console.log(2) }, randomTime() );
-
-setTimeout( () => { console.log(3) }, randomTime() );
-
-// When will the number be logged?
 const randomWait = (number) => {
-  setTimeout( () => { console.log(number) }, randomTime() );
+  return new Promise( (resolve, reject) => {
+    setTimeout( () => { resolve(number) }, randomTime() );
+  })
 }
 
+randomWait(5).then( (number) => {
+  console.log(number);
+})
+
+// Promise All - Array of promises
+Promise.all([
+  randomWait(1),
+  randomWait(2),
+  randomWait(3)
+  ]).then( (numbers) =>{
+    console.log(numbers);
+  })
+
+//Promise Race  - Whichever promises finishes first
+Promise.race([
+  randomWait('Kora')
+  randomWait('Barrett')
+]).then( winner => {
+  console.log(winner);
+})
